950c10bd858708f5b7c114183311bfeaaa851fdf
maddojka <yuriy.soroko@gmail.com>
2024-02-05 20:17:04 +0300

    New file + modified Lesson01

--- src/lesson01/Lesson01.java
+++ src/lesson01/Lesson01.java
@@ -2,6 +2,6 @@ package lesson01;
 
 public class Lesson01 {
     public static void main(String[] args) {
-        System.out.println("Lesson 01");
+        System.out.println("Lesson 01 - task 1");
     }
 }
